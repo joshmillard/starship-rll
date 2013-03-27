c555c141a48412ed5710d8a83aed5aeb96a1e142
joshmillard <josh@joshmillard.com>
2013-03-26 17:23:08 -0700

    bridge now has skeleton of menu function

--- viewscreen.lua
+++ viewscreen.lua
@@ -26,6 +26,19 @@ function load()
 	alien = alien_list[math.random(#alien_list)]
 	alienvessel = alienvessel_list[math.random(#alienvessel_list)]
 
+	-- define some menu contents
+	menu_root = { selection = 1, items = { 
+			{"DIPLOMACY", "do_diplomacy_menu()"},
+			{"TACTICAL", "do_tactical_menu()"},
+			{"ENGINEERING", "do_engineering_menu()"},
+			{"NAVIGATION", "do_navigation_menu()"},
+			{"COMMUNICATIONS", "do_communications_menu()"},
+			{"SENSORS", "do_sensors_menu()"}
+		} }
+
+	-- which menu are we viewing?
+	active_menu = menu_root
+
 end
 
 
@@ -47,9 +60,57 @@ function draw()
 	love.graphics.setColor(180,180,180,255)
 	love.graphics.draw(viewscreenwindow, 0, 0)
 
+	-- then draw a menu
+	draw_active_menu()
+
 end
 
 
 function keypressed(key)
+	if key == "down" then
+		go_next_menu_item()
+	elseif key == "up" then
+		go_prev_menu_item()
+	elseif key == "right" then
+		do_menu_item()
+	end	
+end
 
+
+function draw_active_menu()
+	-- oh hey neat, you can use # around an evaluated expression? 
+	for i=1,#(active_menu.items) do
+		if active_menu.selection == i then
+			love.graphics.setColor(90,30,30,255)
+		else
+			love.graphics.setColor(0,0,0,255)
+		end
+		love.graphics.print(active_menu.items[i][1], 20, 160 + (i*8))
+	end
+	love.graphics.setColor(90,30,30,255)
+	love.graphics.print("--", 10, 160 + (active_menu.selection*8))
+end
+
+
+-- cycle to the next item in the current active menu
+function go_next_menu_item()
+	active_menu.selection = active_menu.selection + 1
+	if active_menu.selection > #(active_menu.items) then
+		active_menu.selection = 1
+	end
+end
+
+
+-- and prev
+function go_prev_menu_item()
+	active_menu.selection = active_menu.selection - 1
+	if active_menu.selection < 1 then
+		active_menu.selection = #(active_menu.items)
+	end	
+end
+
+
+-- execute function tied to menu item
+function do_menu_item()
+	-- ha ha sucker
 end
